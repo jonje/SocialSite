1d4fe47cae8467753accfd1ef9a98874f54ac95c
Jonathan Jensen <haiyun211@gmail.com>
2014-02-14 14:00:12 -0700

    Updated

---
+++ jensenj_Social_Site_V2/Models/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jensenj_Social_Site_V2.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string Layout { get; set; }
+        public string Description { get; set; }
+        public ArrayList BookList = new ArrayList();
+        public ArrayList MovieList = new ArrayList();
+        public ArrayList FoodList = new ArrayList();
+
+    }
+}
